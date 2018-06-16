75f0833a9a7a0dfe6d97e1e9a3cdb73a1e163b47
0xmegalodon <dung.thanh@autonomous.nyc>
2018-06-16 22:15:15 +0700

    -: update code feed exchange

--- src/components/handshakes/exchange/Feed/FeedExchange.jsx
+++ src/components/handshakes/exchange/Feed/FeedExchange.jsx
@@ -77,46 +77,287 @@ class FeedExchange extends React.PureComponent {
     this.mainColor = 'linear-gradient(-180deg, rgba(0,0,0,0.50) 0%, #303030 0%, #000000 100%)';
   }
 
-  componentDidMount() {
-    // this.rateRef.open();
+  showLoading = () => {
+    this.props.showLoading({ message: ''  });
   }
+
+  hideLoading = () => {
+    this.props.hideLoading();
+  }
+
+  handleOnShake = () => {
+    this.modalRef.open();
+  }
+
+  showAlert = (message) => {
+    this.props.showAlert({
+      message: <div className="text-center">
+        {message}
+      </div>,
+      timeOut: 5000,
+      type: 'danger',
+      callBack: () => {
+      }
+    });
+  }
+
+  checkMainNetDefaultWallet = (wallet) => {
+    const { intl } = this.props;
+    let result = true;
+
+    if (process.env.isProduction && !process.env.isStaging) {
+      if (wallet.network === MasterWallet.ListCoin[wallet.className].Network.Mainnet) {
+        result = true;
+      } else {
+        const message = intl.formatMessage({id: 'requireDefaultWalletOnMainNet'}, {});
+        this.showAlert(message);
+        result = false;
+      }
+    }
+
+    return result;
+  }
+
+  showNotEnoughCoinAlert = (balance, amount, fee, currency) => {
+    const bnBalance = new BigNumber(balance);
+    const bnAmount = new BigNumber(amount);
+    const bnFee = new BigNumber(fee);
+
+    const condition = bnBalance.isLessThan(bnAmount.plus(bnFee));
+
+    if (condition) {
+      const { intl } = this.props;
+      this.props.showAlert({
+        message: <div className="text-center">
+          {intl.formatMessage({ id: 'notEnoughCoinInWallet' }, {
+            amount: formatAmountCurrency(balance),
+            fee: formatAmountCurrency(fee),
+            currency: currency,
+          })}
+        </div>,
+        timeOut: 3000,
+        type: 'danger',
+        callBack: () => {
+        }
+      });
+    }
+
+    return condition;
+  }
+
+
+  // //////////////////////
+
+  shakeOfferItem = async (values) => {
+    console.log('shakeOfferItem', values);
+    this.modalRef.close();
+
+    const { authProfile } = this.props;
+    const { offer } = this;
+
+    const shopType = values.type === EXCHANGE_ACTION.BUY ? EXCHANGE_ACTION.SELL : EXCHANGE_ACTION.BUY;
+
+    const wallet = MasterWallet.getWalletDefault(values.currency);
+
+    if (!this.checkMainNetDefaultWallet(wallet)) {
+      return;
+    }
+
+    if (shopType === EXCHANGE_ACTION.BUY) { // shop buy
+      const balance = await wallet.getBalance();
+      const fee = await wallet.getFee(10, true);
+
+      if (this.showNotEnoughCoinAlert(balance, values.amount, fee, values.currency)) {
+        return;
+      }
+    }
+
+    const offerItem = {
+      type: shopType,
+      currency: values.currency,
+      amount: values.amount,
+      username: authProfile?.name,
+      email: authProfile?.email,
+      contact_phone: authProfile?.phone,
+      contact_info: authProfile?.address,
+      user_address: wallet.address,
+      chat_username: authProfile?.username,
+    };
+
+    this.showLoading();
+    this.props.shakeOfferItem({
+      PATH_URL: `${API_URL.EXCHANGE.OFFER_STORES}/${offer.id}/${API_URL.EXCHANGE.SHAKES}`,
+      METHOD: 'POST',
+      data: offerItem,
+      successFn: this.handleShakeOfferItemSuccess,
+      errorFn: this.handleShakeOfferItemFailed,
+    });
+  }
+
+  handleShakeOfferItemSuccess = async (responseData) => {
+    console.log('handleShakeOfferItemSuccess', responseData);
+
+    const { intl } = this.props;
+    const { data } = responseData;
+    const offerShake = Offer.offer(data);
+    const { currency, type, totalAmount, systemAddress, offChainId } = offerShake;
+    const { offer } = this;
+
+    if (currency === CRYPTO_CURRENCY.ETH) {
+      if (type === EXCHANGE_ACTION.BUY) { // shop buy
+        const amount = totalAmount;
+
+        const wallet = MasterWallet.getWalletDefault(currency);
+        const exchangeHandshake = new ExchangeShopHandshake(wallet.chainId);
+        const result = await exchangeHandshake.initByCustomer(offer.items.ETH.userAddress, amount, offChainId);
+
+        console.log('handleShakeOfferSuccess', result);
+      }
+    } else if (currency === CRYPTO_CURRENCY.BTC) {
+      if (type === EXCHANGE_ACTION.BUY) {
+        const wallet = MasterWallet.getWalletDefault(currency);
+        wallet.transfer(systemAddress, totalAmount, 10).then((success) => {
+          console.log('transfer', success);
+        });
+      }
+    }
+
+    this.hideLoading();
+    const message = intl.formatMessage({ id: 'shakeOfferItemSuccessMassage' }, {
+    });
+
+    this.props.showAlert({
+      message: <div className="text-center">{message}</div>,
+      timeOut: 2000,
+      type: 'success',
+      callBack: () => {
+        // this.props.history.push(URL.HANDSHAKE_ME);
+      },
+    });
+  }
+
+  handleShakeOfferItemFailed = (e) => {
+    this.handleActionFailed(e);
+  }
+
+  handleActionFailed = (e) => {
+    this.hideLoading();
+    // console.log('e', e);
+    this.props.showAlert({
+      message: <div className="text-center">{getErrorMessageFromCode(e)}</div>,
+      timeOut: 3000,
+      type: 'danger',
+      callBack: () => {
+      },
+    });
+  }
+
+  getOfferDistance = () => {
+    const { intl,  ipInfo: { latitude, longitude, country }, location } = this.props;
+    const { offer } = this;
+    // let distanceKm = 0;
+    // let distanceMiles = 0;
+
+    let distanceKm = 0;
+    if (location) {
+      const latLng = location.split(',');
+      distanceKm = getDistanceFromLatLonInKm(latitude, longitude, latLng[0], latLng[1]);
+    }
+
+    return intl.formatMessage({ id: 'offerDistanceContent' }, {
+      distance: getLocalizedDistance(distanceKm, country)
+      // distanceKm: distanceKm > 1 || distanceMiles === 0 ? distanceKm.toFixed(0) : distanceKm.toFixed(3),
+      // distanceMiles: distanceMiles === 0 ? distanceKm.toFixed(0) : distanceMiles.toFixed(1),
+    });
+  }
+
+  getPrices = () => {
+    const { listOfferPrice } = this.props;
+
+    let priceBuyBTC;
+    let priceSellBTC;
+    let priceBuyETH;
+    let priceSellETH;
+
+    if (listOfferPrice) {
+      let offerPrice = getOfferPrice(listOfferPrice, EXCHANGE_ACTION.BUY, CRYPTO_CURRENCY.BTC);
+      priceBuyBTC = offerPrice.price;
+
+      offerPrice = getOfferPrice(listOfferPrice, EXCHANGE_ACTION.SELL, CRYPTO_CURRENCY.BTC);
+      priceSellBTC = offerPrice.price;
+
+      offerPrice = getOfferPrice(listOfferPrice, EXCHANGE_ACTION.BUY, CRYPTO_CURRENCY.ETH);
+      priceBuyETH = offerPrice.price;
+
+      offerPrice = getOfferPrice(listOfferPrice, EXCHANGE_ACTION.SELL, CRYPTO_CURRENCY.ETH);
+      priceSellETH = offerPrice.price;
+    }
+
+    return {
+      priceBuyBTC, priceSellBTC, priceBuyETH, priceSellETH,
+    };
+  }
+
+  getNameShopDisplayed = () => {
+    const { username, itemFlags, items } = this.offer;
+    if (username) { return username; }
+    if (itemFlags.ETH) {
+      const wallet = new Ethereum();
+      wallet.address = items.ETH.userAddress;
+      return wallet.getShortAddress();
+    }
+    if (itemFlags.BTC) {
+      const wallet = new Bitcoin();
+      wallet.address = items.BTC.userAddress;
+      return wallet.getShortAddress();
+    }
+    return '';
+  }
+
+  handleChat = () => {
+    const { chatUsername } = this.props;
+    this.props.history.push(`${URL.HANDSHAKE_ME}/${chatUsername}`);
+  }
+
 
   render() {
     const { offer } = this;
-
     const { review, reviewCount } = this.props;
 
-    console.log('review, reviewCount',review, reviewCount);
+    let coins = [];
 
-    const coins = [
-      {
-        name: 'BTC',
-        priceBuy: 2134343,
-        priceSell: 2323111,
-        color: '#FF880E',
-        icon: iconBtc
-      },
-      {
-        name: 'ETH',
-        priceBuy: 223322,
-        priceSell: 231211,
-        color: 'linear-gradient(-135deg, #CB75ED 0%, #9E53E1 100%)',
-        icon: iconEth
-      },
-    ]
+    const {
+      priceBuyBTC, priceSellBTC, priceBuyETH, priceSellETH,
+    } = this.getPrices();
 
-    // const nameShopDisplayed = this.getNameShopDisplayed();
-    // const currency = offer.fiatCurrency;
-    // const success = offer.transactionCount.success || 0;
-    // const failed = offer.transactionCount.failed || 0;
-    //
-    // const distance = this.getOfferDistance();
+    if (offer.itemFlags.BTC) {
+      let coin = {};
 
-    // const {
-    //   priceBuyBTC, priceSellBTC, priceBuyETH, priceSellETH,
-    // } = this.getPrices();
-    const address = '0x2134134'
-    const distance = '12 km away'
+      coin.name = CRYPTO_CURRENCY.BTC;
+      coin.color = '#FF880E';
+      coin.icon = iconBtc;
+      coin.priceBuy = offer.items.BTC.buyBalance ? formatMoney(priceBuyBTC) : '-';
+      coin.priceSell = offer.items.BTC.sellBalance ? formatMoney(priceSellBTC) : '-';
+
+      coins.push(coin);
+    }
+
+    if (offer.itemFlags.ETH) {
+      let coin = {};
+
+      coin.name = CRYPTO_CURRENCY.ETH;
+      coin.color = 'linear-gradient(-135deg, #CB75ED 0%, #9E53E1 100%)';
+      coin.icon = iconEth;
+      coin.priceBuy = offer.items.ETH.buyBalance ? formatMoney(priceBuyETH) : '-';
+      coin.priceSell = offer.items.ETH.sellBalance ? formatMoney(priceSellETH) : '-';
+
+      coins.push(coin);
+    }
+
+    const currency = offer.fiatCurrency;
+    const address = this.getNameShopDisplayed();
+    const distance = this.getOfferDistance();
+
     return (
       <div>
         <div className="feed-exchange">
@@ -128,8 +369,8 @@ class FeedExchange extends React.PureComponent {
                   return (
                     <span key={index} className="coin-item" style={{ background: color }} onClick={() => console.log('click item')}>
                     <div className="icon-coin"><img src={icon}/></div>
-                    <div className="price"><label>BUY</label><span>{priceBuy}</span></div>
-                    <div className="price"><label>SELL</label><span>{priceSell}</span></div>
+                    <div className="price"><label>BUY</label><span>{priceBuy} {currency}</span></div>
+                    <div className="price"><label>SELL</label><span>{priceSell} {currency}</span></div>
                   </span>
                   )
                 })
@@ -138,11 +379,11 @@ class FeedExchange extends React.PureComponent {
             <div className="info-ex">
               <div>
                 <div className="address">{address}</div>
-                <div className="review">25 reviews</div>
+                <div className="review">{`(${reviewCount} reviews)`}</div>
                 <div className="distance">{distance}</div>
               </div>
               <div className="btn-chat">
-                <button className="btn" onClick={() => console.log('chat')}>
+                <button className="btn" onClick={this.handleChat}>
                   <img src={iconChat} />
                 </button>
               </div>
