f3138a7f6502616120092b1d0310305107464591
Hakawai <vtkimsa@gmail.com>
2018-11-22 15:09:58 +0700

    change coinbase

--- src/guru/pages/WalletCoin/WalletCoin.jsx
+++ src/guru/pages/WalletCoin/WalletCoin.jsx
@@ -16,10 +16,15 @@ const stateConstant = 'sa123456';
 class WalletCoin extends React.Component {
   loginCoinBase = (props) => {
 
+    /*
     const clientId = 'e087d6e310d0fcac821dbbe0ed890e26ea492f380baf276d0f9fdf8150114c8e';
     const redirectUrl = encodeURIComponent('http://localhost:8080/auth/callback');
     const URL = `https://www.coinbase.com/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUrl}&state=${stateConstant}&scope=wallet:accounts:read`;
     window.open(URL);
+    */
+    const { currentProvider: cp } = window.web3;
+    const isToshi = !!cp.isToshi;
+    console.log('Is Toshi:', isToshi);
   }
 
   connect = () => {
