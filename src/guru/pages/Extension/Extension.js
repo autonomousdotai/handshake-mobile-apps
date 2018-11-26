5df115481f21113d79fef753d28da06772db1044
0xTatsu <nghia@autonomous.nyc>
2018-11-26 18:10:51 +0700

    how to play

---
+++ src/guru/pages/Extension/Extension.js
@@ -0,0 +1,189 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Icon from '@/guru/components/Icon/Icon';
+import { URL } from '@/constants';
+
+// Icons
+import LogoWhite from '@/assets/images/pex/extension/logo-white.svg';
+import MacSVG from '@/assets/images/pex/extension/mac.svg';
+import ethToWallet from '@/assets/images/pex/extension/eth-to-wallet.svg';
+import chooseMarket from '@/assets/images/pex/extension/choose-market.svg';
+import makePrediction from '@/assets/images/pex/extension/make-prediction.svg';
+
+import './styles.scss';
+
+function header() {
+  return (
+    <div className="Header">
+      <Icon className="Logo" path={LogoWhite} />
+      <div className="LeftBlock">
+        <h1 className="HeadLine">
+          NINJA <br />
+          CHROME EXTENSION
+        </h1>
+        <h3 className="SubHeadLine">Outsmart the Internet</h3>
+        <p className="DescHeadline">
+          Predict the future of politics, technology, <br />
+          money, sports and anything else you can think of!
+        </p>
+        <button className="btn btn-primary">
+          <i className="fab fa-chrome" />
+          Add to Chrome
+        </button>
+        <p className="OrOpen">
+          or open{' '}
+          <Link to={URL.HANDSHAKE_PREDICTION}>ninja.org/prediction</Link>
+        </p>
+      </div>
+      <div className="RightBlock">
+        <Icon className="Macbook" path={MacSVG} />
+      </div>
+    </div>
+  );
+}
+
+function howToPlay() {
+  return (
+    <div className="HowToPlay">
+      <div className="TitleWrapper">
+        <h4 className="NavTitle">How to play</h4>
+        <h2 className="Title">As EASY AS 1-2-3</h2>
+      </div>
+      <div className="BlockContent">
+        <div className="Item">
+          <div className="ItemImage">
+            <Icon path={ethToWallet} />
+          </div>
+          <p className="ItemTitle">Add ETH to wallet</p>
+          <p className="ItemDesc">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
+            ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas
+            accumsan lacus vel facilisis.
+          </p>
+        </div>
+        <div className="Item">
+          <div className="ItemImage">
+            <Icon path={chooseMarket} />
+          </div>
+          <p className="ItemTitle">Choose a market</p>
+          <p className="ItemDesc">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
+            ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas
+            accumsan lacus vel facilisis.
+          </p>
+        </div>
+        <div className="Item">
+          <div className="ItemImage">
+            <Icon path={makePrediction} />
+          </div>
+          <p className="ItemTitle">Make your prediction</p>
+          <p className="ItemDesc">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
+            ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas
+            accumsan lacus vel facilisis.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function aboutUs() {
+  return (
+    <div className="AboutUs">
+      <div className="TitleWrapper">
+        <h4 className="NavTitle">About us</h4>
+        <h2 className="Title">
+          A DECENTRALIZED, SECURED <br /> & PRIVATE PREDICTION PLATFORM
+        </h2>
+      </div>
+      <p className="Desc">
+        Ninja is a constantly evolving project built by a team of mobile
+        developers, gamers, blockchain engineers and sci-fi writers, side by
+        side a growing community of users from all around the world.
+      </p>
+      <div className="BtnAction">
+        <button className="Join">Join our telegram</button>
+        <button className="Goto">Go to Ninja app</button>
+      </div>
+      <div>Image...</div>
+      <p className="Desc">
+        Ninja runs on top of the Ethereum blockchain. It matches users that
+        support the outcome directly with users that predict otherwise. Their
+        stakes are held in an Escrow smart contract, automatically released to
+        the winners once the result is determined. This means that Ninja never
+        holds funds, that transactions are unstoppable, and payouts are
+        guaranteed.
+      </p>
+    </div>
+  );
+}
+
+function addExtension() {
+  return (
+    <div className="AddExtension">
+      <h2 className="AddTitle">Let&#39;s start predicting today</h2>
+      <p className="Desc">
+        Do not wait any longer. Download this extension now and start predicting
+        and creating on any events you could imagine.
+      </p>
+      <button>Add to Chrome now</button>
+    </div>
+  );
+}
+
+function footer() {
+  return (
+    <div className="Footer">
+      <div className="MainInfo">
+        <div className="Logo" />
+        <p>
+          Ninja is open-source, decentralized software that never holds user
+          data, or user funds. As such, Ninja does not have the power to alter
+          or restrict any actions made on the platform and so cannot be
+          responsible for policing it.
+        </p>
+        <p>
+          By freely choosing to use Ninja, the user accepts sole responsibility
+          for their behavior and agrees to abide by the legalities of their
+          governing jurisdiction. Ninja cannot be liable for legal, monetary or
+          psychological damages should you do something stupid. Never stake more
+          than you are willing to lose. Play safe!
+        </p>
+        <p>
+          Need more information? check out our FAQ and instructions on how to
+          play. <br />
+          Copyright &#9400; 2018. All Rights Reserved. Legal.
+        </p>
+      </div>
+      <div className="SocialLinks">
+        <h2>JOIN US ON</h2>
+        <ul>
+          <li>Facebook</li>
+          <li>Twitter</li>
+          <li>Linkedin</li>
+          <li>Github</li>
+          <li>telegram</li>
+        </ul>
+      </div>
+      <div className="SubScribeEmail" />
+    </div>
+  );
+}
+
+function Extension() {
+  return (
+    <div className="ChromeExtension">
+      {header()}
+      {howToPlay()}
+      {aboutUs()}
+      {addExtension()}
+      {footer()}
+    </div>
+  );
+}
+
+export default Extension;
