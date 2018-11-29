035251ce253f445f41ed02fdb5942c307ad4d471
Hakawai <vtkimsa@gmail.com>
2018-11-29 14:41:05 +0700

    Add restore Wallet to Extension

--- src/pages/TopUp/TopUp.js
+++ src/pages/TopUp/TopUp.js
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types';
 import QRCode from 'qrcode.react';
 import Modal from '@/components/core/controls/Modal';
 import { MasterWallet } from '@/services/Wallets/MasterWallet';
+import { injectIntl } from 'react-intl';
 import CopyIcon from '@/assets/images/icon/icon-copy.svg';
 import RestoreWallet from '@/components/Wallet/RestoreWallet/RestoreWallet';
 import Button from '@/components/core/controls/Button';
+import BackChevronSVGWhite from '@/assets/images/icon/back-chevron-white.svg';
 
 import './TopUp.scss';
 
@@ -70,19 +72,23 @@ class TopUp extends React.Component {
   };
   restoreWallet=(props) => {
     return (
+      <div className="RestoreButton">
       <Button onClick={() => {
         this.modalRestoreRef.open();
         }}
-      >I had a wallet. Restore my wallet.
+      >I have a wallet. Restore my wallet.
       </Button>
+      </div>
     );
   }
   renderModalRestor = () => {
+    const { messages } = this.props.intl;
+    const modalHeaderStyle = {color: "#fff", background: "#546FF7"};
     return (
       <Modal
-        // customBackIcon={BackChevronSVGWhite}
-        // modalHeaderStyle={this.modalHeaderStyle}
-        // title={messages.wallet.action.restore.header}
+        customBackIcon={BackChevronSVGWhite}
+        modalHeaderStyle={modalHeaderStyle}
+        title={messages.wallet.action.restore.header}
         onRef={modal => this.modalRestoreRef = modal}
         onClose={this.closeRestoreWalletAccount}
       >
@@ -107,4 +113,4 @@ class TopUp extends React.Component {
   }
 }
 
-export default TopUp;
+export default injectIntl(TopUp);
