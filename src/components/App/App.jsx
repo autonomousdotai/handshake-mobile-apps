c08c49f640f242d759ef62e39c25e3af1c2791f1
0xTatsu <nghia@autonomous.nyc>
2018-12-05 18:04:57 +0700

    update app structure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,25 +1,36 @@
 import React from 'react';
-// redux
-import { Provider } from 'react-redux';
-import { hot } from 'react-hot-loader';
-import { ConnectedRouter } from 'connected-react-router';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-// app
-import Root from '@/components/App/Root';
-// store
-import store from '@/stores';
-import history from '@/services/history';
+import { initApp } from '@/reducers/app/action';
+import Router from '@/components/Router/Router';
+import I18n from '@/components/App/I18n';
+
+import '@/styles/main';
+import '@/styles/custom-icons/styles.css';
 
 class App extends React.Component {
+  static propTypes = {
+    initApp: PropTypes.func.isRequired
+  }
+
+  componentDidMount() {
+    this.props.initApp();
+  }
+
   render() {
     return (
-      <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <Root />
-        </ConnectedRouter>
-      </Provider>
+      <I18n>
+        <div className="root">
+          <Router />
+        </div>
+      </I18n>
     );
   }
 }
 
-export default hot(module)(App);
+export default connect((state) => {
+  return {
+    router: state.router
+  };
+}, { initApp })(App);
--- src/index.js
+++ src/index.js
@@ -1,41 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { getGasPrice } from '@/utils/gasPrice.js';
-// components
+import { Provider } from 'react-redux';
+import { AppContainer } from 'react-hot-loader';
+import { ConnectedRouter } from 'connected-react-router';
+
 import App from '@/components/App/App';
-import * as OfflinePlugin from 'offline-plugin/runtime';
+import store from '@/stores';
+import history from '@/services/history';
+
+import { getGasPrice } from '@/utils/gasPrice.js';
 
 window.gasPrice = 64;
 getGasPrice();
 
-if (process.env.caches) {
-  OfflinePlugin.install({
-    onUpdateReady() {
-      OfflinePlugin.applyUpdate();
-    },
-    onUpdated() {
-      window.location.reload();
-    }
-  });
-} else {
-  if (window.caches) {
-    window.caches
-      .keys()
-      .then(keyList =>
-        Promise.all(keyList.map(key => window.caches.delete(key)))
-      );
-  }
-  if (navigator.serviceWorker) {
-    navigator.serviceWorker.getRegistrations().then(registrations => {
-      registrations.forEach(sw => {
-        if (/\/sw.js$/.test(sw.active.scriptURL)) {
-          sw.unregister();
-        }
-      });
-    });
-  }
-}
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <Component />
+        </ConnectedRouter>
+      </Provider>
+    </AppContainer>,
+    document.getElementById('app'),
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('app'));
-const root = document.getElementById('root');
-if (root) root.addEventListener('contextmenu', e => e.preventDefault());
+render(App);
+
+// webpack Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('@/components/App/App', () => {
+    render(App);
+  });
+}
